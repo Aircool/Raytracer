db5b2f3a40d1493a7914278c1a9611b3494a3d8e
Caleb Meyer <calebzmeyer@gmail.com>
2016-10-21 19:22:35 -0700

    Moved jitter() to VectorMath.cpp
    As it says above.

--- VectorMath.cpp
+++ VectorMath.cpp
@@ -4,3 +4,11 @@ Vec3 operator*(float scalar, Vec3 V){
 	
 	return Vec3(V[0] * scalar, V[1] * scalar, V[2] * scalar);
 }
+
+Vec3 jitter(float length){
+		
+	float r0 = static_cast <float> (rand()) / static_cast <float> (RAND_MAX);
+	float r1 = static_cast <float> (rand()) / static_cast <float> (RAND_MAX);
+		
+	return Vec3(length * (-0.5 + r0), length * (-0.5 + r1), 0.0);
+}
